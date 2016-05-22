e5a100b706b89c3ea8633f1644667b450b367756
Tony Ta <tonyta.tt@gmail.com>
2016-05-22 14:12:26 -0700

    configure secret_key_base directly
    Rails requires the use of secret_key_base and, by default, it is
    configured per environment. Since we deleted the environments, the
    secret_key_base can be configured directly in `application.rb`.

--- config/application.rb
+++ config/application.rb
@@ -8,9 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Rails140
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.secret_key_base = "x"
   end
 end
 
