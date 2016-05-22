a9710252cbfd9f7d28ffc04375f04d2b3cd4b71a
Tony Ta <tonyta.tt@gmail.com>
2016-05-22 14:16:20 -0700

    delete unused rails components
    Since we are only serving plaintext on a single endpoint, the only Rails
    component we need is ActionController. The `action_controller/railtie`
    is all that is required.

--- config/application.rb
+++ config/application.rb
@@ -1,15 +1,6 @@
 require_relative 'boot'
 
-require 'rails'
-
-require "active_record/railtie"
 require "action_controller/railtie"
-require "action_view/railtie"
-require "action_mailer/railtie"
-require "active_job/railtie"
-require "action_cable/engine"
-require "rails/test_unit/railtie"
-require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
