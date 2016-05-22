0da3eabd61ecc33b2caf68c8975f5778715a88b0
Tony Ta <tonyta.tt@gmail.com>
2016-05-22 14:43:06 -0700

    inline all requires into environment.rb
    `environment.rb` is required by `config.ru`.
    
    `environment.rb` loads `application.rb` which loads `boot.rb`. Then
    `routes.rb` is loaded. We condense this all into `environment.rb`.

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,24 @@
-# Load the Rails application.
-require_relative 'application'
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
+require 'bundler/setup' # Set up gems listed in the Gemfile.
 
-# Initialize the Rails application.
+require "action_controller/railtie"
+
+Bundler.require(*Rails.groups)
+
+module Rails140
+  class Application < Rails::Application
+    config.secret_key_base = "x"
+  end
+end
+
+class HelloController < ActionController::Base
+  def index
+    render plain: "Hello! It's #{ Time.current }"
+  end
+end
+
 Rails.application.initialize!
+
+Rails.application.routes.draw do
+  root to: "hello#index"
+end
